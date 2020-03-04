1b2611e4084176b1dad0d2a19fa7a4e35c27b0cd
Fotis Alexandrou <falexandrou@gmail.com>
2020-03-04 02:06:42 +0200

    clean up

--- config/puma.rb
+++ config/puma.rb
@@ -2,6 +2,8 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['THREAD_COUNT'] || 5)
 threads threads_count, threads_count
 
+preload_app!
+
 rackup      DefaultRackup
 port        ENV['PORT']     || 4000
 environment ENV['RACK_ENV'] || 'development'
