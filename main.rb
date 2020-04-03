ba952bc3a9ad7566a50d47e032a0d8499558f5ec
Fotis Alexandrou <falexandrou@gmail.com>
2020-04-04 00:07:10 +0300

    switch to json - add version

--- main.rb
+++ main.rb
@@ -1,6 +1,19 @@
 require 'sinatra'
+require 'json'
 require 'sinatra/reloader' if development?
 
+before do
+  content_type :json
+end
+
 get '/' do
-  'Hello, world!'
+  {
+    success: true,
+    deployedby: :stackmate,
+    version: '1.0'
+  }
+end
+
+after do
+  response.body = JSON.dump(response.body)
 end
